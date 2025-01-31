5b98ad32e5196594c9c92982afa934beca00064c
Andy Grecu <andy.grecu@orange.com>
2025-01-31 11:47:41 +0200

    initial commit

--- test.js
+++ test.js
@@ -1,28 +1,38 @@
-const assert = require('assert');
-const http = require('http');
-const server = require('./index');
+const assert = require("assert");
+const http = require("http");
+const server = require("./index");
 
 function testServer() {
   return new Promise((resolve, reject) => {
-    http.get('http://localhost:8080/hello', (res) => {
-      let data = '';
+    http
+      .get("http://localhost:8080/hello", (res) => {
+        let data = "";
 
-      // Read response data
-      res.on('data', (chunk) => {
-        data += chunk;
-      });
+        // Read response data
+        res.on("data", (chunk) => {
+          data += chunk;
+        });
 
-      res.on('end', () => {
-        try {
-          assert.strictEqual(res.statusCode, 200, 'Status code should be 200');
-          assert.strictEqual(data, '{"Message" : "Hello world"}', 'Response should be "Hello world"');
-          console.log('Passed');
-          resolve();
-        } catch (error) {
-          reject(error);
-        }
-      });
-    }).on('error', reject);
+        res.on("end", () => {
+          try {
+            assert.strictEqual(
+              res.statusCode,
+              200,
+              "Status code should be 200"
+            );
+            assert.strictEqual(
+              data,
+              '{"Message" : "Hello world"}',
+              'Response should be "Hello world"'
+            );
+            console.log("Passed");
+            resolve();
+          } catch (error) {
+            reject(error);
+          }
+        });
+      })
+      .on("error", reject);
   });
 }
 
@@ -31,7 +41,7 @@ server.listen(8080, async () => {
     await testServer();
     server.close();
   } catch (error) {
-    console.error('Failed :', error.message);
+    console.error("Failed :", error.message);
     server.close();
     process.exit(1);
   }
